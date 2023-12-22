e1b0b9f668d234b00a0066baf56df3cb9ac4950f
genos <yunusemreozturk1@hotmail.com>
2023-12-22 07:45:12 +0300

    splash screen prepared

--- app/src/main/java/com/example/login_signup/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/example/login_signup/ui/screens/SplashScreen.kt
@@ -1,9 +1,46 @@
 package com.example.login_signup.ui.screens
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.login_signup.ui.theme.MyApplicationTheme
 
 @Composable
 fun SplashScreen() {
-    Text("Burada")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.width(64.dp),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SplashScreenLight() {
+    MyApplicationTheme(darkTheme = false) {
+        SplashScreen()
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SplashScreenDark() {
+    MyApplicationTheme(darkTheme = true) {
+        SplashScreen()
+    }
 }
